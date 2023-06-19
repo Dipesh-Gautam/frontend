cc4521b63f89fd7d1cced0516f58c1df83e83561
Dipesh-Gautam <be2019se674@gces.edu.np>
2023-06-19 09:58:21 +0545

    Todo List

---
+++ todo/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+
+
+const App = ()=> {
+  const [userInput,setUserInput] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const addData =  ()=> {
+    setTodos([...todos,userInput]);
+  }
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input type="text" onChange={(event)=> setUserInput(event.target.value)}></input>
+      <button onClick={()=>addData()}>Add</button>
+      <ul>
+        {
+          todos.map((todo,index) => <li key={index}>{todo}</li>)
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
