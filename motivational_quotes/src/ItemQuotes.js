e3f595b1cf2355550ea57f72cd4c459f67c62a04
Dipesh-Gautam <be2019se674@gces.edu.np>
2023-06-19 19:43:21 +0545

    Motivational Quotes App

---
+++ motivational_quotes/src/ItemQuotes.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const ItemQuotes = ({text, author})=> {
+  return (
+    <div>
+      <p>Author : {author}</p>
+      
+      <Item>{text}</Item>
+      
+
+    </div>
+  )
+}
+
+export default ItemQuotes
